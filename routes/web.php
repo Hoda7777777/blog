ec4b907b943406d2c1ed5570b6cd9db639e6396b
Hoda7777777 <hodaoama7777777@gmail.com>
2019-12-11 22:02:02 +0200

    msg

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('file-upload', 'FileUploadController@fileUpload')->name('file.upload');
+Route::post('file-upload', 'FileUploadController@fileUploadPost')->name('file.upload.post');
+
+Route::get('user/create', 'HomeController@create');
+Route::post('user/create', 'HomeController@store');
